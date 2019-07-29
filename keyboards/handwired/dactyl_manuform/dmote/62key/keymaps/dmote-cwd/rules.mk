82a32db4c8d72fda731bcc59c43a7f9e755abbce
CommunistWitchDoctor <communistwitchdoctor@gmail.com>
2019-07-29 13:42:09 -0500

    Update rules.mk

--- keyboards/handwired/dactyl_manuform/dmote/62key/keymaps/dmote-cwd/rules.mk
+++ keyboards/handwired/dactyl_manuform/dmote/62key/keymaps/dmote-cwd/rules.mk
@@ -1,1 +1,6 @@
+# Build-process overrides for the DMOTE.
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+RGBLIGHT_ENABLE = yes       # Needed for the C linker with lighting control.
+COMMAND_ENABLE = no         # Not a good combo with Space Cadet shift.
 LEADER_ENABLE = yes
