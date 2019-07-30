77d56994b031ee3e35041e37192a5937795b0f2d
CommunistWitchDoctor <communistwitchdoctor@gmail.com>
2019-07-30 11:07:49 -0500

    Enable autoshift

--- keyboards/handwired/dactyl_manuform/dmote/62key/keymaps/dmote-cwd/rules.mk
+++ keyboards/handwired/dactyl_manuform/dmote/62key/keymaps/dmote-cwd/rules.mk
@@ -4,3 +4,4 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 RGBLIGHT_ENABLE = yes       # Needed for the C linker with lighting control.
 COMMAND_ENABLE = no         # Not a good combo with Space Cadet shift.
 LEADER_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
