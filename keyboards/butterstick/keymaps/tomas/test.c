5748fc49123ae00d15a9fda683fea0da495f1031
Tomas Denemark <denemark.tomas@gmail.com>
2019-09-11 07:30:24 -0700

    Autoshift tests

--- keyboards/butterstick/keymaps/tomas/test.c
+++ keyboards/butterstick/keymaps/tomas/test.c
@@ -651,7 +651,7 @@ autoshift_dance_impl (const struct Chord *self) {
         *self->state = IDLE;
         break;
     case FINISHED_FROM_ACTIVE:
-        if (*self->counter == 2) {
+        if (*self->counter == 1) {
             key_in (KC_LSFT);
             tap_key (self->value1);
             key_out (KC_LSFT);
@@ -891,6 +891,11 @@ uint8_t state_4 = IDLE;
 const struct Chord chord_4 PROGMEM =
     { H_BOT7 + H_BOT8, QWERTY, &state_4, NULL, NUM, 0, temp_pseudolayer };
 
+uint8_t state_5 = IDLE;
+uint8_t counter_5 = 0;
+const struct Chord chord_5 PROGMEM =
+    { H_BOT1, QWERTY, &state_5, &counter_5, KC_Z, 0, autoshift_dance };
+
 // Register all chords, load chording logic
 const struct Chord *const list_of_chords[] PROGMEM = {
     &chord_0,
@@ -898,6 +903,7 @@ const struct Chord *const list_of_chords[] PROGMEM = {
     &chord_2,
     &chord_3,
     &chord_4,
+    &chord_5,
 
 };
 
@@ -961,7 +967,7 @@ are_hashed_keycodes_in_array (uint32_t keycode_hash) {
 
 void
 kill_one_shots (void) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         // const struct Chord* chord = list_of_chords[i];
         struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
         struct Chord chord_storage;
@@ -981,7 +987,7 @@ kill_one_shots (void) {
 
 void
 process_finished_dances (void) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
         struct Chord chord_storage;
 
@@ -1021,7 +1027,7 @@ deactivate_active_taphold_chords (struct Chord *caller) {
         return;
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
         struct Chord chord_storage;
 
@@ -1054,7 +1060,7 @@ keycodes_buffer_array_min (uint8_t * first_keycode_index) {
 
 void
 remove_subchords (void) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
         struct Chord chord_storage;
 
@@ -1067,7 +1073,7 @@ remove_subchords (void) {
             continue;
         }
 
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 6; j++) {
             if (i == j) {
                 continue;
             }
@@ -1099,7 +1105,7 @@ process_ready_chords (void) {
 
     while (keycodes_buffer_array_min (&first_keycode_index)) {
         // find ready chords
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
             struct Chord chord_storage;
 
@@ -1143,7 +1149,7 @@ process_ready_chords (void) {
         // this should be only one chord
         struct Chord *chord = NULL;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
             struct Chord chord_storage;
 
@@ -1196,7 +1202,7 @@ deactivate_active_chords (uint16_t keycode) {
     uint32_t hash = (uint32_t) 1 << (keycode - SAFE_RANGE);
     bool broken;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
         struct Chord chord_storage;
 
@@ -1328,7 +1334,7 @@ void
 clear (const struct Chord *self) {
     if (*self->state == ACTIVATED) {
         // kill all chords
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             struct Chord *chord_ptr = (struct Chord *) pgm_read_word (&list_of_chords[i]);
             struct Chord chord_storage;
 
@@ -1793,11 +1799,145 @@ test_autoshift_toggle () {
     return 0;
 }
 
+// AS
+
+static char *
+test_autoshift_tap () {
+    char name[] = "autoshift_tap";
+
+    do {
+        uint8_t clear_state = ACTIVATED;
+        struct Chord clear_chord PROGMEM = { 0, QWERTY, &clear_state, NULL, 0, 0, clear };
+        clear_chord.function (&clear_chord);
+    } while (0);
+
+    current_time = 0;
+    history_index = 0;
+
+    for (int j = 0; j < SAFE_RANGE - 1; j++) {
+        keyboard_history[0][j] = 0;
+    }
+    time_history[0] = 0;
+    for (int i = 1; i < 20; i++) {
+        for (int j = 0; j < SAFE_RANGE - 1; j++) {
+            keyboard_history[i][j] = -1;
+        }
+        time_history[i] = -1;
+    }
+
+    process_record_user (BOT1, &pressed);
+    pause_ms (CHORD_TIMEOUT + 1);
+    process_record_user (BOT1, &depressed);
+
+    ASSERT_EQ (UINT, keyboard_history[0][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[0][KC_LSFT], 0);
+
+    ASSERT_EQ (UINT, keyboard_history[1][KC_Z], 1);
+    ASSERT_EQ (UINT, keyboard_history[1][KC_LSFT], 0);
+
+    ASSERT_EQ (UINT, keyboard_history[2][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[2][KC_LSFT], 0);
+
+    printf ("%s " GREEN "PASSED" NC "\n", name);
+    return 0;
+}
+
+static char *
+test_autoshift_hold () {
+    char name[] = "autoshift_hold";
+
+    do {
+        uint8_t clear_state = ACTIVATED;
+        struct Chord clear_chord PROGMEM = { 0, QWERTY, &clear_state, NULL, 0, 0, clear };
+        clear_chord.function (&clear_chord);
+    } while (0);
+
+    current_time = 0;
+    history_index = 0;
+
+    for (int j = 0; j < SAFE_RANGE - 1; j++) {
+        keyboard_history[0][j] = 0;
+    }
+    time_history[0] = 0;
+    for (int i = 1; i < 20; i++) {
+        for (int j = 0; j < SAFE_RANGE - 1; j++) {
+            keyboard_history[i][j] = -1;
+        }
+        time_history[i] = -1;
+    }
+
+    process_record_user (BOT1, &pressed);
+    pause_ms (CHORD_TIMEOUT + 1);
+    pause_ms (3 * (DANCE_TIMEOUT + 1));
+    process_record_user (BOT1, &depressed);
+
+    ASSERT_EQ (UINT, keyboard_history[0][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[0][KC_LSFT], 0);
+
+    ASSERT_EQ (UINT, keyboard_history[1][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[1][KC_LSFT], 1);
+
+    ASSERT_EQ (UINT, keyboard_history[2][KC_Z], 1);
+    ASSERT_EQ (UINT, keyboard_history[2][KC_LSFT], 1);
+
+    ASSERT_EQ (UINT, keyboard_history[3][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[3][KC_LSFT], 1);
+
+    ASSERT_EQ (UINT, keyboard_history[4][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[4][KC_LSFT], 0);
+
+    printf ("%s " GREEN "PASSED" NC "\n", name);
+    return 0;
+}
+
+static char *
+test_autoshift_hold_off () {
+    char name[] = "autoshift_hold_off";
+
+    do {
+        uint8_t clear_state = ACTIVATED;
+        struct Chord clear_chord PROGMEM = { 0, QWERTY, &clear_state, NULL, 0, 0, clear };
+        clear_chord.function (&clear_chord);
+    } while (0);
+
+    current_time = 0;
+    history_index = 0;
+
+    for (int j = 0; j < SAFE_RANGE - 1; j++) {
+        keyboard_history[0][j] = 0;
+    }
+    time_history[0] = 0;
+    for (int i = 1; i < 20; i++) {
+        for (int j = 0; j < SAFE_RANGE - 1; j++) {
+            keyboard_history[i][j] = -1;
+        }
+        time_history[i] = -1;
+    }
+
+    autoshift_mode = 0;
+
+    process_record_user (BOT1, &pressed);
+    pause_ms (CHORD_TIMEOUT + 1);
+    pause_ms (3 * (DANCE_TIMEOUT + 1));
+    process_record_user (BOT1, &depressed);
+
+    ASSERT_EQ (UINT, keyboard_history[0][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[0][KC_LSFT], 0);
+
+    ASSERT_EQ (UINT, keyboard_history[1][KC_Z], 1);
+    ASSERT_EQ (UINT, keyboard_history[1][KC_LSFT], 0);
+
+    ASSERT_EQ (UINT, keyboard_history[2][KC_Z], 0);
+    ASSERT_EQ (UINT, keyboard_history[2][KC_LSFT], 0);
+
+    printf ("%s " GREEN "PASSED" NC "\n", name);
+    return 0;
+}
+
 // TODO:
 // KL
 // KM
 // KK
-// AS
 // DF
 // TO
 // LOCK
@@ -1834,6 +1974,12 @@ all_tests () {
 
     mu_run_test (test_autoshift_toggle);
 
+    mu_run_test (test_autoshift_tap);
+
+    mu_run_test (test_autoshift_hold);
+
+    mu_run_test (test_autoshift_hold_off);
+
     return 0;
 }
 
@@ -1846,7 +1992,7 @@ main (int argc, char **argv) {
     } else {
         printf ("\n" GREEN "ALL TESTS PASSED" NC "\n");
     }
-    printf ("Tests run: %d / %d\n", tests_run, 11);
+    printf ("Tests run: %d / %d\n", tests_run, 14);
 
     return result != 0;
 }
